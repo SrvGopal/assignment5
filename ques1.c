fb2d9d99817683cda2738cd98382502b4d283014
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-26 15:12:45 +0530

    Create ques1.c

---
+++ ques1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n,i ;
+    printf("ENter the number ");
+    scanf("%d",&n);
+    
+    while(n >= i){
+        printf("%d ",i);
+        i++;
+
+    }
+
+}
