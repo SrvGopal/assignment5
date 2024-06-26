aaa04f6212d8e5e5c58703d4331fc4a8403ff00a
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-26 15:45:04 +0530

    Create ques8.c

---
+++ ques8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  int n,sum;
+  printf("Enter the number ");
+  scanf("%d",&n);
+   
+  n = (n+1)/2;
+  
+  sum = n*n;
+ 
+  printf("The sum of the numbers is %d",sum);
+
+}
